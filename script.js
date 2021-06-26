f11e16271959104eb37da157d75de29cb18db5b3
oley-projects <oley.for.git@gmail.com>
2021-06-26 19:08:45 +0300

    html, styling project; game rules

---
+++ script.js
@@ -0,0 +1,8 @@
+const rulesBtn = document.getElementById('rules-btn'),
+      closeBtn = document.getElementById('close-btn'),
+      rules = document.getElementById('rules');
+
+
+// Rules and close event handlers
+rulesBtn.addEventListener('click', () => rules.classList.add('show'));
+closeBtn.addEventListener('click', () => rules.classList.remove('show'));
