b1379e5c46e16604bd2fa079bc2cecabf266455e
oley-projects <oley.for.git@gmail.com>
2021-06-27 15:53:34 +0300

    move elements functionality

--- script.js
+++ script.js
@@ -87,15 +87,109 @@ function drawBricks() {
     });
 }
 
+// Move paddle on canvas
+function movePaddle() {
+    paddle.x += paddle.dx;
+
+    // Wall detection
+    if (paddle.x + paddle.w > canvas.width) {
+        paddle.x = canvas.width - paddle.w;
+    }
+
+    if (paddle.x < 0) {
+        paddle.x = 0;
+    }
+}
+
+// Move ball on canvas
+function moveBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    // Wall collision (right/left)
+    if (ball.x + ball.size > canvas.width ||ball.x - ball.size < 0) {
+        ball.dx *= -1;
+    }
+
+    // Wall collision (bot/bottom)
+    if (ball.y + ball.size > canvas.height ||ball.y - ball.size < 0) {
+        ball.dy *= -1;
+    }
+
+    // Paddle collision
+    if (ball.x - ball.size > paddle.x && 
+        ball.x + ball.size < paddle.x + paddle.w && 
+        ball.y + ball.size > paddle.y) 
+        {
+            ball.dy = -ball.speed;
+        }
+
+    // Brick collision
+    bricks.forEach(column => {
+        column .forEach(brick => {
+            if (brick.visible) {
+                if (// left brick side check
+                    ball.x - ball.size > brick.x &&
+                    // right brick side check
+                    ball.x + ball.size < brick.x + brick.w &&
+                    // top brick side check
+                    ball.y + ball.size > brick.y &&
+                    // bottom brick side check
+                    ball.y - ball.size < brick.y + brick.h) {
+                        
+                        ball.dy *= -1;
+                        brick.visible = false;
+                }
+            }
+        });   
+    });
+}
+
 // Draw everything
 function draw() {
+    // Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     drawBall();
     drawPaddle();
     drawScore();
-    drawBricks()
+    drawBricks();
 }
 
-draw();
+// Update canvas drawing and animation
+function update() {
+    movePaddle();
+    moveBall();
+
+    // Draw everything
+    draw();
+
+    requestAnimationFrame(update);
+}
+
+update();
+
+// Keydown event
+function keyDown(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        paddle.dx = paddle.speed;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        paddle.dx = -paddle.speed;
+    }
+
+}
+
+// Keyup event
+function keyUp(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight' || 
+        e.key === 'Left' || e.key === 'ArrowLeft') {
+            paddle.dx = 0;
+    }
+}
+
+// Keyboard event handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
